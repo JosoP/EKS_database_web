6757b911f94574ccbdce88fff5ffa038f7fafd30
JosoP <jozkoprivarcak@gmail.com>
2019-12-06 11:23:22 +0100

    Init of project

---
+++ EKS_database_web/Data/UserDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace EKS_database_web.Data
+{
+    public class UserDbContext : IdentityDbContext
+    {
+        public UserDbContext(DbContextOptions<UserDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
